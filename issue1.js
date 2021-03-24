53325431bf58953b59340de5eefa15ff455a0760
lucas150296 <65991522+lucas150296@users.noreply.github.com>
2021-03-24 18:57:19 -0300

    fim

---
+++ issue1.js
@@ -0,0 +1,14 @@
+function somas(x, y) {
+  let soma = x + y;
+  console.log("soma " + soma);
+
+  let sub = x - y;
+  console.log("subtração " + sub);
+
+  let divisao = x / y;
+  console.log("divisao " + divisao);
+
+  let multiplicacao = x * y;
+  console.log("multiplicação " + multiplicacao);
+}
+somas(10, 15);
